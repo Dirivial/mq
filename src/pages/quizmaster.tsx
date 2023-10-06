2eb12bfc7edbe7f8ee8b753b9d489046175476bd
Dirivial <alexander.kadeby@gmail.com>
2023-10-06 12:46:01 +0200

    Checking if I can fetch from website

--- src/pages/quizmaster.tsx
+++ src/pages/quizmaster.tsx
@@ -4,8 +4,100 @@ import { useState } from "react";
 
 import { api } from "~/utils/api";
 
+const exampleQuiz = {
+  name: "Example Quiz",
+  questions: [
+    {
+      name: "1: What is the name of this song?",
+      url: "https://api.deezer.com/track/3135556",
+      answers: [
+        {
+          name: "Around The World",
+          correct: false,
+        },
+        {
+          name: "Harder Better Faster Stronger",
+          correct: true,
+        },
+        {
+          name: "Get Lucky",
+          correct: false,
+        },
+        {
+          name: "Da Funk",
+          correct: false,
+        },
+      ],
+    },
+    {
+      name: "2: What is the name of this song?",
+      url: "https://api.deezer.com/track/2514681",
+      answers: [
+        {
+          name: "Something for Money",
+          correct: false,
+        },
+        {
+          name: "Nothing for Money",
+          correct: false,
+        },
+        {
+          name: "Money for Something",
+          correct: false,
+        },
+        {
+          name: "Money for Nothing",
+          correct: true,
+        },
+      ],
+    },
+  ],
+};
+
 export default function QuizMaster() {
   const { data: sessionData } = useSession();
+
+  const [currentQuestion, setCurrentQuestion] = useState(-1);
+
+  /*
+  Pritority 1: Connect to deezer API and play a couple of songs.
+
+  Priority 2: Create example quiz with 3 questions and play a song for each of them.
+
+  Priority 3: Automatically go to the next question after the song has finished playing, and some thinking time.
+  */
+
+  const startQuiz = () => {
+    setCurrentQuestion(0);
+
+    const url = exampleQuiz.questions.at(0).url;
+    console.log("Fetching with url: " + url);
+    // Use the fetch function to make the GET request
+    fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    })
+      .then((response) => {
+        // Check if the response status is OK (200)
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        // Parse the response body as JSON (assuming it's JSON data)
+        return response.json();
+      })
+      .then((data) => {
+        // Handle the data returned from the server
+        console.log(data);
+      })
+      .catch((error) => {
+        // Handle any errors that occurred during the fetch request
+        console.error("Fetch error:", error);
+      });
+  };
 
   return (
     <>
@@ -20,7 +112,10 @@ export default function QuizMaster() {
             QuizMaster
           </h1>
 
-          <button className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20">
+          <button
+            onClick={startQuiz}
+            className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+          >
             Starta Quiz
           </button>
 
@@ -32,6 +127,7 @@ export default function QuizMaster() {
             Logga ut
           </button>
         </div>
+        <div>{currentQuestion >= 0 && <span>{exampleQuiz.name}</span>}</div>
       </main>
     </>
   );
