77b74b538bafc87cc7aec580fba230cb018364fe
Dirivial <alexander.kadeby@gmail.com>
2023-10-13 20:02:42 +0200

    Minor UX improvements

--- src/pages/quiz.tsx
+++ src/pages/quiz.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 import { SkipToNext, Play, Pause, AddSongsToQueue } from "~/utils/musicPlayer";
@@ -103,6 +104,7 @@ export default function Quiz() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [gameOver, setGameOver] = useState(false);
   const [songsLoaded, setSongsLoaded] = useState(false);
+  const [showQuestion, setShowQuestion] = useState(false);
 
   useEffect(() => {
     const nextQuestion = () => {
@@ -123,6 +125,7 @@ export default function Quiz() {
       if (timePassed > 30) {
         console.log("Time passed");
         setTimePassed(0);
+        setShowQuestion(false);
         nextQuestion();
       } else {
         setTimePassed((timePassed) => timePassed + 0.1);
@@ -137,7 +140,9 @@ export default function Quiz() {
       if (currentIndex === 0) {
         Play()
           .then((v) => {
+            setTimePassed(0);
             console.log("Playing", v);
+            setShowQuestion(true);
           })
           .catch(() => {
             console.log("Error playing");
@@ -147,6 +152,7 @@ export default function Quiz() {
           .then(() => {
             console.log("Skipped to next song");
             setTimePassed(0);
+            setShowQuestion(true);
           })
           .catch(() => console.log("Error skipping to next song"));
       }
@@ -180,9 +186,12 @@ export default function Quiz() {
       </Head>
       <main className=" flex min-h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-6xl font-extrabold tracking-tight text-white sm:text-[3rem]">
+          <Link
+            href={"/quizmaster"}
+            className="text-6xl font-extrabold tracking-tight text-white sm:text-[3rem]"
+          >
             {exampleQuiz.name}
-          </h1>
+          </Link>
         </div>
         {gameOver ? (
           <div>
@@ -194,30 +203,34 @@ export default function Quiz() {
               (Statistik...)
             </h3>
           </div>
-        ) : (
-          songsLoaded && (
-            <div>
-              <h1 className="text-center text-2xl font-bold text-white">
-                {currentQuestion?.name}
-              </h1>
+        ) : showQuestion ? (
+          <div>
+            <h1 className="text-center text-2xl font-bold text-white">
+              {currentQuestion?.name}
+            </h1>
 
-              <div className="mt-10 grid grid-cols-2 gap-2">
-                {currentQuestion?.answers.map((answer) => (
-                  <button className="text btn h-24 text-lg" key={answer.name}>
-                    <h3>{answer.name}</h3>
-                  </button>
-                ))}
-              </div>
-
-              <div className="flex flex-col p-10">
-                <progress
-                  className="progress mx-auto w-96"
-                  value={timePassed}
-                  max="30"
-                ></progress>
-              </div>
+            <div className="mt-10 grid grid-cols-2 gap-2">
+              {currentQuestion?.answers.map((answer) => (
+                <button className="text btn h-24 text-lg" key={answer.name}>
+                  <h3>{answer.name}</h3>
+                </button>
+              ))}
             </div>
-          )
+
+            <div className="flex flex-col p-10">
+              <progress
+                className="progress mx-auto w-96"
+                value={timePassed}
+                max="30"
+              ></progress>
+            </div>
+          </div>
+        ) : (
+          <div>
+            <h1 className="text-center text-2xl font-bold text-white">
+              Gör er redo!
+            </h1>
+          </div>
         )}
       </main>
     </>
