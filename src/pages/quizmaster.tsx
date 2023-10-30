fa4e0df9e923f306a2a4382b9af52dbac920f909
Dirivial <alexander.kadeby@gmail.com>
2023-10-31 00:31:46 +0100

    More weird bugs popping up in untouched code???

--- src/pages/quizmaster.tsx
+++ src/pages/quizmaster.tsx
@@ -40,8 +40,14 @@ export default function QuizMaster() {
     outline?: boolean;
   }
 
-  function ActionButton({ onClick, label, outline = false }: ActionButtonProps) {
-    const buttonClass = `btn btn-wide ${outline ? 'btn-outline' : ''} btn-primary`;
+  function ActionButton({
+    onClick,
+    label,
+    outline = false,
+  }: ActionButtonProps) {
+    const buttonClass = `btn btn-wide ${
+      outline ? "btn-outline" : ""
+    } btn-primary`;
     return (
       <button onClick={onClick} className={buttonClass}>
         {label}
@@ -54,33 +60,43 @@ export default function QuizMaster() {
       <Head>
         <title>MQ - QuizMaster</title>
         <meta name="description" content="Dashboard för quizmasters." />
-        <meta name="apple-music-developer-token" content={env.NEXT_PUBLIC_APPLE_DEVELOPER_TOKEN} />
+        <meta
+          name="apple-music-developer-token"
+          content={env.NEXT_PUBLIC_APPLE_DEVELOPER_TOKEN}
+        />
         <meta name="apple-music-app-name" content="My Cool App" />
         <meta name="apple-music-app-build" content="1.0" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Script
         src="https://js-cdn.music.apple.com/musickit/v3/musickit.js"
-        onLoad={tryAuthorize} // Assuming tryAuthorize is defined elsewhere
+        onLoad={() => void tryAuthorize()} // Assuming tryAuthorize is defined elsewhere
       />
-      <main className="min-h-screen flex items-center justify-center bg-background">
+      <main className="bg-background flex min-h-screen items-center justify-center">
         <div className="container mx-auto p-4 text-center">
-          <h1 className="text-6xl font-extrabold md:text-7xl mb-12">
+          <h1 className="mb-12 text-6xl font-extrabold md:text-7xl">
             QuizMaster
           </h1>
           <div className="flex flex-col items-center gap-4">
             <ActionButton onClick={startQuiz} label="Starta Quiz" />
             <ActionButton onClick={openRoom} label="Öppna ett Rum" />
-            {!isAuthorized && <ActionButton onClick={tryAuthorize} label="Anslut till Apple Music" />}
-            <div className="w-full my-4">
+            {!isAuthorized && (
+              <ActionButton
+                onClick={() => void tryAuthorize()}
+                label="Anslut till Apple Music"
+              />
+            )}
+            <div className="my-4 w-full">
               <div className="divider">eller</div>
             </div>
-            <ActionButton onClick={signOut} label="Logga ut" outline />
+            <ActionButton
+              onClick={() => void signOut()}
+              label="Logga ut"
+              outline
+            />
           </div>
         </div>
       </main>
     </>
   );
 }
-  
-
