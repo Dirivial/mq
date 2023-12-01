a29870a5248e6fbc22b721155db1205f90c9c22a
Dirivial <alexander.kadeby@gmail.com>
2023-12-01 02:05:22 +0100

    Opening a room with a specific quiz added

--- src/pages/quizmaster.tsx
+++ src/pages/quizmaster.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { env } from "~/env.mjs"
+import { env } from "~/env.mjs";
 import { useRouter } from "next/navigation";
 import { ConfigureMusicKit } from "~/utils/musicPlayer";
 import Script from "next/script";
@@ -17,8 +17,12 @@ export default function QuizMaster() {
 
   const openRoom = () => {
     const roomId = Math.random().toString(36).substring(7);
-    console.log("Opening a new room with id " + roomId + "...");
     router.push("/room/" + roomId + "?qid=" + qid);
+  };
+
+  const openRoomWithQuizId = (quizId: number) => {
+    const roomId = Math.random().toString(36).substring(7);
+    router.push("/room/" + roomId + "?qid=" + quizId);
   };
 
   const tryAuthorize = async () => {
@@ -31,49 +35,71 @@ export default function QuizMaster() {
   };
 
   const CreateQuizCard = () => {
-    return(
-    <div className="card card-compact w-80 bg-base-100 shadow-xl">
-      <figure><img src="quizcard.png" alt="Shoes"/></figure>
-      <div className="flex card-body items-center items-center justify-center">
-        <h2 className="flex card-title">Skapa ett helt eget quiz!</h2>
-        <p>Skräddarsay den ultimata quizupplevelsen.</p>
-        <button className="btn btn-primary w-full max-w-xs" onClick={createQuiz}>QuizMaker</button>
+    return (
+      <div className="card card-compact w-80 bg-base-100 shadow-xl">
+        <figure>
+          <img src="quizcard.png" alt="Shoes" />
+        </figure>
+        <div className="card-body flex items-center items-center justify-center">
+          <h2 className="card-title flex">Skapa ett helt eget quiz!</h2>
+          <p>Skräddarsay den ultimata quizupplevelsen.</p>
+          <button
+            className="btn btn-primary w-full max-w-xs"
+            onClick={createQuiz}
+          >
+            QuizMaker
+          </button>
+        </div>
       </div>
-    </div>
     );
   };
 
   const OpenRoomCard = () => {
-    return(
-    <div className="card card-compact w-80 bg-base-100 shadow-xl text-center">
-      <figure><img src="quizroom.png" alt="Shoes" /></figure>
-      <div className="flex card-body items-center items-center justify-center">
-        <h2 className="flex card-title">Öppna ett rum Quizrum!</h2>
-        <p>Öppna ett rum, välj ett quiz och låt folk joina.</p>
-        <button className="btn btn-primary w-full max-w-xs" onClick={openRoom}>Quizroom</button>
+    return (
+      <div className="card card-compact w-80 bg-base-100 text-center shadow-xl">
+        <figure>
+          <img src="quizroom.png" alt="Shoes" />
+        </figure>
+        <div className="card-body flex items-center items-center justify-center">
+          <h2 className="card-title flex">Öppna ett rum Quizrum!</h2>
+          <p>Öppna ett rum, välj ett quiz och låt folk joina.</p>
+          <button
+            className="btn btn-primary w-full max-w-xs"
+            onClick={openRoom}
+          >
+            Quizroom
+          </button>
+        </div>
       </div>
-    </div>
     );
   };
 
   const UserQuizzesList = () => {
     const quizzes = [
-      { id: 1, title: '80-talsmusik Quiz' },
-      { id: 2, title: 'Rockmusikens Historia' },
-      { id: 3, title: 'Rockmusikens Historia' },
+      { id: 1, title: "80-talsmusik Quiz" },
+      { id: 2, title: "Rockmusikens Historia" },
+      { id: 3, title: "Rockmusikens Historia" },
 
       // Lägg till fler quizzes här
     ];
-  
+
     return (
-      <div className="prose overflow-x-auto flex items-center flex-col pt-10">
+      <div className="prose flex flex-col items-center overflow-x-auto pt-10">
         <h2>Mina skapade quiz</h2>
         <div className="flex flex-col items-center gap-4">
           {quizzes.map((quiz) => (
-            <div key={quiz.id} className="card card-compact w-96 bg-base-100 shadow-xl">
+            <div
+              key={quiz.id}
+              className="card card-compact w-96 bg-base-100 shadow-xl"
+            >
               <div className="card-body">
                 <h5 className="card-title">{quiz.title}</h5>
-                <button className="btn btn-secondary">Visa Quiz</button>
+                <button
+                  className="btn btn-secondary"
+                  onClick={() => openRoomWithQuizId(quiz.id)}
+                >
+                  Visa Quiz
+                </button>
               </div>
             </div>
           ))}
@@ -121,18 +147,16 @@ export default function QuizMaster() {
         onLoad={() => void tryAuthorize()} // Assuming tryAuthorize is defined elsewhere
       />
       <main className="flex flex-auto flex-col items-center justify-center">
-
-          <div className="flex flex-col md:flex-row items-center gap-4">
-            <CreateQuizCard/>
-            {!isAuthorized && (
-              <ActionButton
-                onClick={() => void tryAuthorize()}
-                label="Anslut till Apple Music"
-              />
-            )}
-          </div>
-          <UserQuizzesList />
-
+        <div className="flex flex-col items-center gap-4 md:flex-row">
+          <CreateQuizCard />
+          {!isAuthorized && (
+            <ActionButton
+              onClick={() => void tryAuthorize()}
+              label="Anslut till Apple Music"
+            />
+          )}
+        </div>
+        <UserQuizzesList />
       </main>
     </>
   );
